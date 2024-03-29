026ecd289866f3a1a66cc5237c3a2a966c5fb90d
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-03-28 19:56:35 -0500

    feat(property): Add property and location entities - classes

--- src/property/entities/property.entity.ts
+++ src/property/entities/property.entity.ts
@@ -1,1 +1,13 @@
-export class Property {}
+import { Location } from '../../aux_entities/location.entity';
+
+// clase Property
+export class Property {
+    id: string;
+    name: string;
+    location: Location;
+    rooms: number;
+    bathrooms: number;
+    area: number;
+    cost_per_night: number; // costo por noche
+    max_people: number; // max gente que puede alojar
+}
