71fd987dc41a9bd1d938929948eb82f0b66b2c8a
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-03-29 15:34:12 -0500

    feat (property): Add some objects in the properties array

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -3,10 +3,33 @@ import {v4 as uuid} from 'uuid'
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 import { Property } from './entities/property.entity';
+import { Location } from '../aux_entities/location.entity';
+import { PropertyType } from 'src/enums/propertyType.enum';
 
 @Injectable()
 export class PropertyService {
-  private properties: Property[] = []
+  private properties: Property[] = [
+    {
+      id: uuid(),
+      type: PropertyType.Apartment,
+      location: new Location(),
+      rooms: 3,
+      bathrooms: 3,
+      area: 20,
+      cost_per_night: 1000, // costo por noche
+      max_people: 8, // max gente que puede alojar
+    },
+    {
+      id: uuid(),
+      type: PropertyType.Apartment,
+      location: new Location(),
+      rooms: 5,
+      bathrooms: 2,
+      area: 35,
+      cost_per_night: 1400, // costo por noche
+      max_people: 10, // max gente que puede alojar
+    },
+  ]
 
   create(createPropertyDto: CreatePropertyDto) {
     try {
