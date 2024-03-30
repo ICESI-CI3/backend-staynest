75d1dca1613e3441a46f6b5fa0b3605bca40e932
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-03-30 15:36:45 -0500

    add structure for auth an user

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    readonly email: string;
+
+    @IsString()
+    readonly password: string;
+
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly role: string;
+
+}
