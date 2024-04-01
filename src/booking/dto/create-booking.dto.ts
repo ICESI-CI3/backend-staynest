638011bf594ed4c6bc1f1e1b3f5ed218945cab03
Svak <svak@Svaks-MacBook-Pro.local>
2024-04-01 05:42:06 -0500

    booking dto

--- src/booking/dto/create-booking.dto.ts
+++ src/booking/dto/create-booking.dto.ts
@@ -1,1 +1,53 @@
-export class CreateBookingDto {}
+
+import { IsNumber, IsString, IsDate, IsBoolean } from "class-validator";
+import { IsPropertyType } from "src/pipes/isPropertyType.pipe";
+
+export class CreateBookingDto {
+    @IsString()
+    readonly booking_id: string;
+
+    @IsDate()
+    readonly check_in: Date;
+
+    @IsDate()
+    readonly check_out: Date;
+
+    @IsString()
+    readonly property_type: string;
+
+    @IsString()
+    readonly property_id: string
+
+    @IsString()
+    readonly id: string;
+
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly last_name: string;
+
+    @IsString()
+    readonly email: string;
+
+    @IsString()
+    readonly phone_number: string;
+
+    @IsString()
+    readonly address: string;
+
+    @IsNumber()
+    readonly num_people: number;
+
+    @IsNumber()
+    readonly payment_method: number;
+
+    @IsBoolean()
+    readonly is_paid: boolean;  
+
+    @IsBoolean()
+    readonly is_confirmed: boolean; 
+
+   
+
+}
