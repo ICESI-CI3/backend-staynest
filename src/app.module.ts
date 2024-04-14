36831339bed8d38082951c4fa42c79a45adf8f8a
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-14 09:28:02 -0500

    add typeorm, but postgres not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,20 +11,34 @@ import { RolesGuard } from './auth/guards/roles.guard';
 import { AuthGuard } from './auth/guards/auth.guard';
 
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
     isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,  // Be cautious with this in production
+      autoLoadEntities: true,
+    }),
     PropertyModule,
     BookingModule,
     UserModule,
     AuthModule,
-    ReportsModule],
+    ReportsModule,
+    ],
   controllers: [AppController],
   providers: [AppService, 
     AuthGuard],
+  exports: [TypeOrmModule]
 })
 export class AppModule  {
 
