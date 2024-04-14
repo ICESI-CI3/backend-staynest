16e283cae0697317f984fdbb25f6a8fc76b83b47
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-14 07:59:31 -0500

    implement passport authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,22 +4,22 @@ import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './guards/auth.guard';
-import { Role } from 'src/enums/role.enum';
 import { RolesGuard } from './guards/roles.guard';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
 
 @Global()
 @Module({
   imports: [
     forwardRef(() => UserModule),
     JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '500s' },
-      }),
-      inject: [ConfigService],
+
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '6h' },
+        }),
+        
     }),
 
   ],
