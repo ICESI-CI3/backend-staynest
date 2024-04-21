b03ce8a6e050f7eb589be0370881441ad25e65b6
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-21 18:39:53 -0500

    Fix formatting issues in app.module.ts and add logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { AuthGuard } from './auth/guards/auth.guard';
 
 async function bootstrap() {
+  console.log("Hey you");
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_USER);
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
 }
 bootstrap();
