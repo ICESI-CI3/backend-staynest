6018959178f4d6f87bcba3758c5f66b3ad6a946c
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-22 09:07:12 -0500

    add report structure yes

--- src/booking/entities/booking.entity.ts
+++ src/booking/entities/booking.entity.ts
@@ -1,38 +1,15 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Property } from 'src/property/entities/property.entity';
 import { PropertyType } from '../../enums/propertyType.enum';
-import { PaymentMethod } from '../../enums/paymentMethod.enum';
-
-@Entity('Booking')
 export class Booking {
-
-
-    @PrimaryGeneratedColumn('uuid')
     id: string;
-
-    @Column('date', {nullable: false})
     check_in: Date;
-
-    @Column('date', {nullable:false})
     check_out: Date;
-
-    @Column('text', {nullable:false})
     property_type: PropertyType;
-
-    @Column('uuid', {nullable:false})
-    property_id: string;
-
-    @Column('uuid', {nullable:false})
+    property: Property;
     user_id: string;
-
-    @Column('numeric', {nullable:false})
     num_people: number;
-
-    @Column('text',{nullable:false})
-    payment_method: PaymentMethod;
-
-    @Column('boolean')
-    is_paid: boolean;
-
-    @Column('boolean')
+    payment_method: string; 
+    is_paid: boolean; 
     is_confirmed: boolean; 
+    cost_per_night: number;
 }
