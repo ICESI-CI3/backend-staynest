7e7524b42fcbccc1ce05fb390a9e187af317e47e
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-22 16:39:42 -0500

    modify unit tests of user iwu

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,41 +1,111 @@
 import { User } from './entities/user.entity';
 import { Role } from '../enums/role.enum';
+import { UserController } from './user.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthModule } from '../auth/auth.module';
+import { AppModule } from '../app.module';
 
 describe('User Entity', () => {
-    let user: User;
+    let controller: UserController;
 
-    beforeEach(() => {
-        user = new User();
-    });
+    let users = [
+        { id: 'u1', email: 'user1@example.com', password: 'pass123', name: 'User One', role: Role.USER },
+        { id: 'u2', email: 'user2@example.com', password: 'pass123', name: 'User Two', role: Role.ADMIN },
+        { id: 'u3', email: 'user3@example.com', password: 'pass123', name: 'User Three', role: Role.OWNER }
+    ];
 
-    it('should have a uuid id', () => {
-        user.id = '123e4567-e89b-12d3-a456-426614174000';
-        expect(typeof user.id).toBe('string');
-    });
+    const mockUserService = {
+        create: jest.fn((userDto) => ({
+          id: 'u' + Math.floor(Math.random() * 100),
+          ...userDto
+        })),
+    
+        findAll: jest.fn(() => users),
+    
+        findOne: jest.fn((id) => users.find(user => user.id === id)),
+    
+        update: jest.fn((id, updateUserDto) => ({
+          id: id,
+          ...updateUserDto,
+        })),
+    
+        remove: jest.fn((id) => users = users.filter(user => user.id !== id))
+      }
 
-    it('should have a password', () => {
-        user.password = 'password123';
-        expect(user.password).toBe('password123');
-    });
+      beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          controllers: [UserController],
+          providers: [UserService],
+          imports: [AuthModule, AppModule]
+        }).overrideProvider(UserService)
+        .useValue(mockUserService)
+        .compile();
+    
+        controller = module.get<UserController>(UserController);
+      });
 
-    it('should have a unique email', () => {
-        user.email = 'test@example.com';
-        expect(user.email).toBe('test@example.com');
-    });
+      it('should be defined', () => {
+        expect(controller).toBeDefined();
+      });
+    
+      it('should create a user', async () => {
+        const dto = {
+          email: 'newuser@example.com',
+          password: 'password123',
+          name: 'New User',
+          role: Role.USER
+        };
+    
+        const result = await controller.create(dto); // Usa await para resolver la promesa
 
-    it('should have a name', () => {
-        user.name = 'Test User';
-        expect(user.name).toBe('Test User');
-    });
+            expect(result).toEqual({
+                id: expect.any(String),
+                ...dto
+            });
 
-    it('should have a role', () => {
-        user.role = Role.ADMIN;
-        expect(user.role).toBe(Role.ADMIN);
+    
+        expect(mockUserService.create).toHaveBeenCalledWith(dto);
+        expect(mockUserService.create).toHaveBeenCalledTimes(1);
+      });
+    
+      it('should get a user by ID', () => {
+        expect(controller.findOne('u1')).toEqual({
+          id: 'u1',
+          email: 'user1@example.com',
+          password: 'pass123',
+          name: 'User One',
+          role: Role.USER
+        });
+    
+        expect(mockUserService.findOne).toHaveBeenCalledWith('u1');
+        expect(mockUserService.findOne).toHaveBeenCalledTimes(1);
+      });
+    
+      it('should get all users', () => {
+        expect(controller.findAll()).toEqual(users);
+        expect(mockUserService.findAll).toHaveBeenCalled();
+      });
+    
+    it('should update a user', () => {
+        const updateUserDto: UpdateUserDto = {
+            id: 'u1',
+            email: 'updateduser@example.com',
+            name: 'Updated Name'
+        };
+        expect(controller.update('u1', updateUserDto)).toEqual({
+            id: 'u1',
+            ...updateUserDto
+        });
+        expect(mockUserService.update).toHaveBeenCalledWith('u1', updateUserDto);
     });
+    
+      it('should remove a user', () => {
+        controller.remove('u1');
+        expect(mockUserService.remove).toHaveBeenCalledWith('u1');
+      });
+    
 
-    it('should lowercase and trim email before insert', () => {
-        user.email = '  TEST@EXAMPLE.COM  ';
-        user.checkFieldsBeforeInsert();
-        expect(user.email).toBe('test@example.com');
-    });
+    
 });
