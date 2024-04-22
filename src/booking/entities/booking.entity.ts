c9a3e760ed9c882a770cc25bd0fb45fd48de92d8
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-22 09:07:12 -0500

    add report structure

--- src/booking/entities/booking.entity.ts
+++ src/booking/entities/booking.entity.ts
@@ -1,1 +1,16 @@
-export class Booking {}
+import { Property } from 'src/property/entities/property.entity';
+import { Location } from '../../aux_entities/location.entity';
+import { PropertyType } from '../../enums/propertyType.enum';
+export class Booking {
+    id: string;
+    check_in: Date;
+    check_out: Date;
+    property_type: PropertyType;
+    property: Property;
+    user_id: string;
+    num_people: number;
+    payment_method: string; 
+    is_paid: boolean; 
+    is_confirmed: boolean; 
+    cost_per_night: number;
+}
