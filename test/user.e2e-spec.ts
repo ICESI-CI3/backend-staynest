4ac445a45004aa8a47e80f4011d4a991a4cb61e0
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-23 18:45:14 -0500

    ya dieron los e2e jiji jaja

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpCode, HttpStatus, INestApplication } from '@nestjs/common';
+import  {v4 as uuid} from 'uuid';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -16,6 +17,23 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
+  it('/user/register (POST) should create a user', async () => {
+    const createUserDto = {
+      email: 'test@example5.com',
+      password: 'testPassword',
+      name: 'TestUser',
+      role: 'OWNER'
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(createUserDto)
+      .expect(201);
+
+   
+    userId = response.body.id; 
+  });
+
   it('/user (GET) should return all users', async () => {
     await request(app.getHttpServer())
       .get('/user')
@@ -25,7 +43,29 @@ describe('UserController (e2e)', () => {
       });
   });
 
+  it('/user/:id (PATCH) should update a user', async () => {
+    const updateUserDto = {
+      name: 'Updated Name'
+    };
 
+    await request(app.getHttpServer())
+      .patch(`/user/${userId}`)
+      .send(updateUserDto)
+      .expect(HttpCode)
+      .then(response => {
+        expect(response.body).toHaveProperty('name', 'Updated Name');
+      });
+  });
+
+
+  it('/user/:id (DELETE) should remove a user', async () => {
+    await request(app.getHttpServer())
+      .delete(`/user/${userId}`)
+      .expect(200);
+  });
+
+
+      
   afterAll(async () => {
     await app.close();
   });
