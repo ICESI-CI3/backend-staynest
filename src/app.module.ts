337e4c131800f1d7d90ce9a90ca04a2770bb9ef5
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-04-23 16:04:49 -0500

    feat (Property): Correct error in create property dto please

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,7 @@ import { redisStore } from 'cache-manager-redis-yet';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       ssl: {
