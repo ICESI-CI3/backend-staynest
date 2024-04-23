761cd78fa2f197d6817838232d78ca69aa646306
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-23 16:56:45 -0500

    remove unnecesary test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -16,27 +16,6 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
-  // it('/user/register (POST) should create a user', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/user/register')
-  //     .send({
-  //       email: 'test@example.com',
-  //       password: 'password',
-  //       name: 'Test User',
-  //       role: 'USER'
-  //     })
-  //     .expect(HttpStatus.CREATED);
-
-  //   userId = response.body.id;  // Store user ID for use in subsequent tests.
-  //   expect(response.body).toEqual({
-  //     id: expect.any(String),
-  //     email: 'test@example.com',
-  //     name: 'Test User',
-  //     role: 'USER'
-  //     // Omit password from the response check for security reasons
-  //   });
-  // });
-
   it('/user (GET) should return all users', async () => {
     await request(app.getHttpServer())
       .get('/user')
@@ -46,21 +25,6 @@ describe('UserController (e2e)', () => {
       });
   });
 
-  // it('/user/:id (PATCH) should update a user', async () => {
-  //   await request(app.getHttpServer())
-  //     .patch(`/user/${userId}`)
-  //     .send({ name: 'Updated Name' })
-  //     .expect(HttpStatus.OK)
-  //     .then(response => {
-  //       expect(response.body.name).toEqual('Updated Name');
-  //     });
-  // });
-
-  // it('/user/:id (DELETE) should delete a user', async () => {
-  //   await request(app.getHttpServer())
-  //     .delete(`/user/${userId}`)
-  //     .expect(HttpStatus.OK);
-  // });
 
   afterAll(async () => {
     await app.close();
