c2a37a19cd5794e535d8c3ef0a9bde17e5ef2f01
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-04-23 16:04:49 -0500

    feat (Property): Correct error in create property dto please

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { CommonModule } from './common/common.module';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true, 
       autoLoadEntities: true,
