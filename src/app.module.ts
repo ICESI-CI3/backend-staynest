ce9c79d52c460b7b26dfb30eefe15ed8a42b7540
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-04-23 16:04:49 -0500

    feat (Property): Correct error in create property dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { SeedModule } from './seed/seed.module';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true, 
       autoLoadEntities: true,
