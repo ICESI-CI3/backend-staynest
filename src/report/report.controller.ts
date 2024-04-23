f19eefa7c2028797bf6216f89e2df48f9988dd7e
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-23 09:03:22 -0500

    delete unnecesary files

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ReportService } from './report.service';
-import { CreateReportDto } from './dto/create-report.dto';
-import { UpdateReportDto } from './dto/update-report.dto';
+
 
 @Controller('report')
 export class ReportController {
@@ -12,10 +11,6 @@ export class ReportController {
     return this.reportService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
-    return this.reportService.update(+id, updateReportDto);
-  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
