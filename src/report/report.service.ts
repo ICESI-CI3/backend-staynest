edb408d33157165e9f69edb41c7445ebf2ac85e9
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-04-23 16:33:49 -0500

    add report module

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -252,55 +252,6 @@ export class ReportService {
     return userActivity;
   }
  
-
-
-
-//   async generateDetailedOccupancyReport(): Promise<any> {
-//     const properties = await this.propertyRepository.find({ relations: ['bookings'] });
-//     return properties.map(property => {
-//         const monthlyOccupancy = property.bookings.reduce((acc, booking) => {
-//             const month = booking.check_in.getMonth(); // Get month as a number (0-11)
-//             if (!acc[month]) {
-//                 acc[month] = { bookedDays: 0, totalDays: 0 };
-//             }
-//             const duration = (booking.check_out.getTime() - booking.check_in.getTime()) / (1000 * 3600 * 24); // Duration in days
-//             acc[month].bookedDays += duration;
-//             acc[month].totalDays += new Date(booking.check_in.getFullYear(), month + 1, 0).getDate(); // Total days in the month
-//             return acc;
-//         }, {});
-
-//         return {
-//             propertyId: property.id,
-//             occupancyByMonth: Object.keys(monthlyOccupancy).map(month => ({
-//                 month,
-//                 occupancyRate: (monthlyOccupancy[month].bookedDays / monthlyOccupancy[month].totalDays * 100).toFixed(2) + '%'
-//             }))
-//         };
-//     });
-// }
-
-// async generateUserActivityReport(): Promise<any> {
-//     const users = await this.userRepository.find({ relations: ['bookings'] });
-//     return users.map(user => {
-//         const activityDetails = user.bookings.reduce((acc, booking) => {
-//             acc.totalBookings++;
-//             acc.totalSpent += booking.cost_per_night * ((booking.check_out.getTime() - booking.check_in.getTime()) / (86400000)); // Assuming cost_per_night is stored in Booking
-//             acc.preferredProperties[booking.property_type] = (acc.preferredProperties[booking.property_type] || 0) + 1;
-//             return acc;
-//         }, { totalBookings: 0, totalSpent: 0, preferredProperties: {} });
-
-//         return {
-//             userId: user.id,
-//             email: user.email,
-//             totalBookings: activityDetails.totalBookings,
-//             totalSpent: activityDetails.totalSpent.toFixed(2),
-//             preferredProperties: activityDetails.preferredProperties
-//         };
-//     });
-// }
-
-
-
   findOne(id: number) {
     return `This action returns a #${id} report`;
   }
