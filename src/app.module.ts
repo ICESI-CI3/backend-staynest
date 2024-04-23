18d5190912a5d0a72fd0994594fa395aab8234e3
Dylan Bermudez Cardona <dyez1110@gmail.com>
2024-04-23 15:07:24 -0500

    feat (Property): Add property unit tests in .spec file yoyo

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,7 @@ import { redisStore } from 'cache-manager-redis-yet';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       ssl: {
